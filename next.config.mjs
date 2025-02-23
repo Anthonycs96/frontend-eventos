2ab4b54fe34ec1cf2b5742880df2fbdcc833fb18
Anthonycs96 <anthonycs.dev@gmail.com>
2025-02-22 20:23:57 -0500

    Fix next/font/google issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true, // Activar modo estricto para detectar errores en desarrollo
     images: {
         domains: [
             "i.pinimg.com",
@@ -10,7 +12,9 @@ const nextConfig = {
             "instagram.flim18-1.fna.fbcdn.net", // Added the missing domain
         ],
     },
-}
+    experimental: {
+        appDir: true, // Para soportar la nueva estructura de Next.js (opcional)
+    }
+};
 
-export default nextConfig
-
+export default nextConfig;
