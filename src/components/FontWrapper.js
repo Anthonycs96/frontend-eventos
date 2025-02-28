904610df07792bbac38720bb897a69054cbe8ca1
Anthonycs96 <anthonycs.dev@gmail.com>
2025-02-28 01:22:28 -0500

    actualizacion de fuentes

--- src/components/FontWrapper.js
+++
@@ -1,7 +0,0 @@
-"use client"; // Asegura que este componente es de cliente
-
-import { dancingScript } from "@/app/fontConfig";
-
-export default function FontWrapper({ children }) {
-    return <div className={dancingScript.className}>{children}</div>;
-}
